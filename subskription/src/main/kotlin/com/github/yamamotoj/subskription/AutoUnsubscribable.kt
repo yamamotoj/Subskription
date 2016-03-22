72f24cb43413b57344ea02494800a7109bcf0955
yamamotoj <jumpei.yamamoto@gmail.com>
2016-03-22 23:21:49 +0900

    apply KDoc format documentation.

--- subskription/src/main/kotlin/com/github/yamamotoj/subskription/AutoUnsubscribable.kt
+++ subskription/src/main/kotlin/com/github/yamamotoj/subskription/AutoUnsubscribable.kt
@@ -6,68 +6,68 @@ import rx.Subscription
 interface AutoUnsubscribable {
 
     /**
-     * Extension function for a {@link Observable} to add {@link Subscription} to this {@code AutoUnsubscribable}.
-     * The {@link Subscription} whill be subscribed on {@code unsubscribe()}.
+     * Extension function for a [Observable] to add [Subscription] to this [AutoUnsubscribable].
+     * The [Subscription] will be subscribed on [unsubscribe].
      */
-    fun <T> Observable<T>.autoUnsubscribe() :Observable<T>
+    fun <T> Observable<T>.autoUnsubscribe(): Observable<T>
 
     /**
-     * Extension function for a {@link Observable} to add {@link Subscription} to this {@code AutoUnsubscribable} with specified {@code key}.
-     * The {@link Subscription} whill be subscribed on {@code unsubscribe(key:Any)} for the key, and on {@code unsubscribe()} as well.
+     * Extension function for a {@link Observable} to add [Subscription] to this [AutoUnsubscribable] with specified key.
+     * The [Subscription] will be subscribed on [unsubscribe] for the key, and on [unsubscribe] as well.
      */
-    fun <T> Observable<T>.autoUnsubscribe(key:Any) :Observable<T>
+    fun <T> Observable<T>.autoUnsubscribe(key: Any): Observable<T>
 
     /**
-     * Extension function for a {@link Subscription} to add itself to this {@code AutoUnsubscribable}.
-     * The {@link Subscription} whill be subscribed on {@code unsubscribe()}.
+     * Extension function for a [Subscription] to add itself to this [AutoUnsubscribable].
+     * The [Subscription] will be subscribed on [unsubscribe].
      */
-    fun Subscription.autoUnsubscribe():Unit
+    fun Subscription.autoUnsubscribe(): Unit
 
     /**
-     * Extension function for a {@link Subscription} to add itself to this {@code AutoUnsubscribable} with specified {@code key}.
-     * The {@link Subscription} will be unsubscribed on {@code unsubscribe(key:Any)} for the key, and on {@code unsubscribe()} as well.
+     * Extension function for a [Subscription] to add itself to this [AutoUnsubscribable] with specified key.
+     * The [Subscription] will be unsubscribed on [unsubscribe] for the key, and on [unsubscribe] as well.
      */
-    fun Subscription.autoUnsubscribe(key:Any):Unit
+    fun Subscription.autoUnsubscribe(key: Any): Unit
 
 
     /**
-     * Add {@link Subscription} to this {@code AutoUnsubscribable}.
-     * The {@link Subscription} will be unsubscribed on {@code unsubscribe(key:Any)} for the key, and on {@code unsubscribe()} as well.
+     * Add [Subscription] to this [AutoUnsubscribable].
+     * The [Subscription] will be unsubscribed on [unsubscribe] for the key, and on [unsubscribe] as well.
      */
-    fun addSubscription(subscription:Subscription):Unit
+    fun addSubscription(subscription: Subscription): Unit
 
     /**
-     * Add {@link Subscription} to this {@code AutoUnsubscribable}.
-     * The {@link Subscription} will be unsubscribed on {@code unsubscribe(key:Any)} for the key, and on {@code unsubscribe()} as well.
+     * Add [Subscription] to this [AutoUnsubscribable].
+     * The [Subscription] will be unsubscribed on [unsubscribe] for the key, and on [unsubscribe] as well.
      */
-    fun addSubscription(subscription: Subscription, key: Any):Unit
+    fun addSubscription(subscription: Subscription, key: Any): Unit
 
     /**
      * Unsubscribe subscriptions for specified key.
-     * This will call {@code unsubscribe(key:Any)} for added {@code AutoUnsubscribable} by {@code addAutoUnsubscribable()}.
+     * This will call [unsubscribe] for added [AutoUnsubscribable] by [addAutoUnsubscribable].
      */
-    fun unsubscribe(key:Any):Unit
+    fun unsubscribe(key: Any): Unit
 
 
     /**
      * Unsubscribe all subscriptions.
      * This will unsubscribe subscriptions with key as well.
-     * This will call {@code unsubscribe()} for added {@code AutoUnsubscribable} by {@code addAutoUnsubscribable()}.
-     * This will remove oll {@code AutoUnsubscribable}s.
-     * When this {@code AutoUnsubscribable} is added to parent by {@code addAutoUnsubscribable(autoUnsubscribable: AutoUnsubscribable)}, this will removed.
+     * This will call [unsubscribe] for added [AutoUnsubscribable] by [addAutoUnsubscribable].
+     * This will remove all [AutoUnsubscribable]s.
+     * When this [AutoUnsubscribable] is added to parent by [addAutoUnsubscribable], this will removed.
      */
-    fun unsubscribe():Unit
+    fun unsubscribe(): Unit
 
     /**
-     * Add {@code AutoUnsubscribable} to this {@code AutoUnsubscribable}.
-     * When {@code unsubscribe()} is called for parent {@code AutoUnsubscribable}, {@code unsubscribe()} is callded for add {@code AutoUnsubscribable} with or without key.
-     * The added {@code AutoUnsubscribable} is removed from parent when {@code unsubscribe()} is called for parent {@code AutoUnsubscribable} without specified key.
-     * When {@code unsubscribe()} is called for added {@code AutoUnsubscribable} without specified key, it remove itself from parent {@code AutoUnsubscribable} as well.
+     * Add [AutoUnsubscribable] to this [AutoUnsubscribable].
+     * When [unsubscribe] is called for parent [AutoUnsubscribable], [unsubscribe] is callded for add [AutoUnsubscribable] with or without key.
+     * The added [AutoUnsubscribable] is removed from parent when [unsubscribe] is called for parent [AutoUnsubscribable] without specified key.
+     * When [unsubscribe] is called for added [AutoUnsubscribable] without specified key, it remove itself from parent [AutoUnsubscribable] as well.
      */
-    fun addAutoUnsubscribable(autoUnsubscribable: AutoUnsubscribable):Unit
+    fun addAutoUnsubscribable(autoUnsubscribable: AutoUnsubscribable): Unit
 
     /**
-     * Remove {@code AutoUnsubscribable} from parent {@code AutoUnsubscribable}.
+     * Remove [AutoUnsubscribable] from parent [AutoUnsubscribable].
      */
-    fun removeAutoUnsubscribable(autoUnsubscribable: AutoUnsubscribable):Unit
+    fun removeAutoUnsubscribable(autoUnsubscribable: AutoUnsubscribable): Unit
 }
